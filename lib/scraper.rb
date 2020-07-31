526c48b4b65badae003cd47a7972d2621b2a24a7
Ethan <eleetyson@gmail.com>
2020-07-31 13:57:12 -0400

    build Scraper class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,16 @@ class Scraper
 
   def self.scrape_page
     doc = Nokogiri::HTML(open("https://news.ycombinator.com/"))
-    binding.pry
+    posts = doc.css(".storylink")
+
+    post_arr = []
+    posts.each do |post|
+      title = post.children.text.chars.select(&:ascii_only?).join
+      binding.pry
+
+      new_post = { :title => title, }
+      post_arr.push(new_post)
+    end
   end
 
 end
